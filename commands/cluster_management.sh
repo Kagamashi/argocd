61f4847dd565b15aebd2a8effafd68a8be5f5eb5
Mikolaj Borecki <mikolaj.borecki@lhsystems.com>
2024-10-02 19:12:36 +0200

    Deleted: commands/cluster_management.sh

--- commands/cluster_management.sh
+++
@@ -1,7 +0,0 @@
-# CLUSTER MANAGEMENT
-argocd cluster add <CONTEXT_NAME>
-# context name corresponds to kubernetes context in local kubeconfig
-
-argocd cluster list
-
-argocd cluster rm <CONTEXT_NAME>
